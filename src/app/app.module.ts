9216c88e22045fd29ee407e57ccfc273bfcba619
guilherme.cirineu <guilhermecirineu@hotmail.com>
2023-06-12 18:30:02 -0300

    https://github.com/gcirineu18/Angular-tests.git

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './page/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
