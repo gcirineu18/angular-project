e4337641d8becc22f5db9f900017bcc9ff4ab522
guilherme.cirineu <guilhermecirineu@hotmail.com>
2023-10-25 18:05:07 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './page/cart/cart.component';
 import { SaleComponent } from './pages/sale/sale.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CartComponent,
-    SaleComponent
+    SaleComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Hero} from '../hero'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+
+  /*The page no longer displays properly because it was 
+  changed the hero from a string to an object. */
+  hero :Hero = {
+     id: 1,
+    name: 'Windstorm'
+  } 
+}
