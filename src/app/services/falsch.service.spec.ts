0490e043f2ccf68c6ed7b1e69b9a48f6016a207f
gcirineu18 <guilhermecirineu@hotmail.com>
2023-12-14 10:20:06 -0300

    post test implemented

--- src/app/services/falsch.service.spec.ts
+++ src/app/services/falsch.service.spec.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 
 
-import { of } from 'rxjs/internal/observable/of';
+import { of,throwError } from 'rxjs/internal/observable/of';
 import { FalschService } from './falsch.service';
 
 describe('FalschService', () => {
@@ -10,7 +11,8 @@ describe('FalschService', () => {
 
     httpClientSpy={
       // mocking a function
-      get: jest.fn()
+      get: jest.fn(),
+      post: jest.fn()
     };
     
     service = new FalschService(httpClientSpy);
@@ -30,6 +32,78 @@ describe('FalschService', () => {
      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
      expect(httpClientSpy.get).toHaveBeenCalledWith(url)
 
-
   })
+
+  it('should getDataV2 throw error', (done: () =>any)=>{
+    const errorResponse = new HttpErrorResponse({
+      error:'test 404 error',
+      status: 404, statusText: 'Not Found.'
+    });
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    jest.spyOn(httpClientSpy, 'get').mockReturnValue(throwError(() => errorResponse));
+    
+    //triggering the method:
+    // As it is of type Observable, subscribe is needed
+    service.getDataV2().subscribe(
+      {
+        next: data => console.log(data),
+
+        error: error => {
+          expect(error.message).toContain('test 404 error')
+          done();
+        }
+
+
+      }
+    );
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+    expect(httpClientSpy.get).toHaveBeenCalledWith(url)
+
+ })
+
+
+ it('should test getDataV2', (done: () =>any)=>{
+  const res = 'Schlüssel-Wort';
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  jest.spyOn(httpClientSpy, 'get').mockReturnValue(of(res));
+  
+  //triggering the method:
+  // As it is of type Observable, subscribe is needed
+  service.getDataV2().subscribe(
+    {
+      next: data =>{
+        expect(data).toEqual(res);
+        // If you miss to call the done callback, then
+        // the test does not know when the response comes, so
+        // it will fail with the timeout error
+        done();
+      },
+      error: error => console.log(error)
+
+
+    }
+  );
+  expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+  expect(httpClientSpy.get).toHaveBeenCalledWith(url)
+
+})
+
+// Post call
+it('should test postDataV1', () =>{
+  const command = 'testing';
+  const res = '现在我什么都不知道';
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  jest.spyOn(httpClientSpy, 'post').mockReturnValue(of(res));
+
+  service.postDataV1(command);
+
+  expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
+  
+})
+
 });
+
+
+
+
+
--- src/app/services/falsch.service.ts
+++ src/app/services/falsch.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, catchError } from 'rxjs';
 
 
 
@@ -23,8 +23,32 @@ export class FalschService {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
     return this.http.get(url).pipe(
-      tap((data: any)=> console.log('Data Fetched :: ', data))
+      tap((data: any)=> console.log('Data Fetched :: ', data)),
+      catchError(this.handleError('Fail to fetch data.'))
     );
+  }
+
+  private handleError<T>(operation='operation'){
+     return(error:HttpErrorResponse): Observable<T> =>{
+
+      console.log(error)
+      const message = `server returned code ${error.status} with body "${error.error}" `
+      
+      throw new Error(`${operation} failed: ${message}`)
+    }
+     
+  }
+
+
+  postDataV1(data: any):Observable<any>{
+
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+    const httpOptions = {
+      Headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+
+    return this.http.post(data,url,httpOptions);
   }
 
 
