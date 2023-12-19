3c4849ec1ad75638a545c56adea9a73a0e371206
guilherme.cirineu <guilhermecirineu@hotmail.com>
2023-12-19 16:03:03 -0300

    att tests

--- src/app/data/data.component.spec.ts
+++ src/app/data/data.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataComponent } from './data.component';
 import { FalschService } from '../services/falsch.service';
+import { of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('DataComponent', () => {
   let component: DataComponent;
@@ -27,10 +29,35 @@ describe('DataComponent', () => {
 
     fixture = TestBed.createComponent(DataComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should getServiceData set serviceData', () =>{
+     const expRes = {
+       name:"Tatsächlich Erstellung"
+     };
+
+     jest.spyOn(fakeServiceMock,'getDataV1').mockReturnValue(of(expRes));
+      fixture.detectChanges();
+
+      expect(component.serviceData.name).toBe(expRes.name);
+  });
+
+
+  it('should getServiceData set errorMessage', () =>{
+      const errorResponse = new  HttpErrorResponse({
+        error: 'test 404',
+        status: 404, statusText: 'Not Found'
+     
+      })
+      
+      jest.spyOn(fakeServiceMock,'getDataV1').mockReturnValue(throwError(() => errorResponse));
+
+      component.getServiceData();
+      expect(component.errorMessage).toBe('Not Found')
+  })
 });
